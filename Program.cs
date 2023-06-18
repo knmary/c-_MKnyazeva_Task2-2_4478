f4bd1bcad01b143988af20b6875c74a392a62ffc
Maria Knyazeva <m.knyazeva@mail.ru>
2023-06-19 00:04:12 +0400

    Program.cs for Task2-2 added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+int ReadInt (string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32 (Console.ReadLine());
+    return value;
+}
+
+bool Validate3sing (int number)
+{
+    if (number >= 100)
+    {
+        return true;
+    }
+
+    System.Console.WriteLine("третьей цифры нет");
+    return  false;
+}
+
+int num  = ReadInt("Введите число >> ");
+if (Validate3sing(num))
+{
+    string NumberText = Convert.ToString(num);
+    Console.WriteLine("третья цифра >> " + NumberText[2]);
+}
